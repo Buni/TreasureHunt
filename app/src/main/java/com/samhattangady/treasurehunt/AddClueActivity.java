f97f33b3ab503bf60a2dcbc0502512243c695821
samhattangady <samhattangady@gmail.com>
2017-01-10 14:38:20 +0530

    google place picker+autocomplete done

--- app/src/main/java/com/samhattangady/treasurehunt/AddClueActivity.java
+++ app/src/main/java/com/samhattangady/treasurehunt/AddClueActivity.java
@@ -1,25 +1,56 @@
 package com.samhattangady.treasurehunt;
 
+import android.Manifest;
 import android.app.Fragment;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
 
-public class AddClueActivity extends FragmentActivity implements  OnMapReadyCallback {
+public class AddClueActivity extends FragmentActivity
+        implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    private Marker marker;
+    private TextView latlongview;
+    private Location mLastLocation;
+    private LatLng mapCenter;
+    private CameraPosition cameraPosition;
+    private static final int PLACE_PICKER_REQUEST = 1;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +64,127 @@ public class AddClueActivity extends FragmentActivity implements  OnMapReadyCall
         mapFragment.getView().setLayoutParams(mapParams);
         mapFragment.getMapAsync(this);
 
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
-        ImageView imageView = (ImageView) findViewById(R.id.image_clue);
-        Picasso.with(imageView.getContext())
-                .load("https://www.drupal.org/files/project-images/Stamen%20Watercolor.png")
-                .into(imageView);
+
+        latlongview = (TextView) findViewById(R.id.lat_long);
+        latlongview.setText("lat long will appear here.");
+
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        Log.v("OnMapReady", "Map is ready apparently.");
-        // Add a marker in Bangalore, and move the camera.
-        LatLng bangalore = new LatLng(12.9716, 77.5946);
-        mMap.addMarker(new MarkerOptions().position(bangalore).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bangalore, 10));
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+            latlongview.setText("Location enabled");
+        }
+
+        // Center of map is in Bangalore if we cannot find most recent location
+        mapCenter = new LatLng(12.9716, 77.5946);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mapCenter = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        }
+        cameraPosition = CameraPosition.builder()
+                .target(mapCenter)
+                .zoom(13)
+                .build();
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 1, null);
+    }
+
+    public void SetLatLong(View view) {
+        latlongview.setText(marker.getPosition().toString());
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            mapCenter = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        }
+        cameraPosition = CameraPosition.builder()
+                .target(mapCenter)
+                .zoom(13)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition), 1, null);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    public void openPlacePicker (View view) throws GooglePlayServicesNotAvailableException, GooglePlayServicesRepairableException {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+        builder.setLatLngBounds(new LatLngBounds(mMap.getCameraPosition().target, mMap.getCameraPosition().target));
+        startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place selectedPlace = PlacePicker.getPlace(this, data);
+                if (marker == null) {
+                    marker = mMap.addMarker(new MarkerOptions().position(selectedPlace.getLatLng()));
+                }
+                marker.setPosition(selectedPlace.getLatLng());
+                cameraPosition = CameraPosition.builder()
+                        .target(mapCenter)
+                        .zoom(13)
+                        .build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.builder()
+                                                                         .target(marker.getPosition())
+                                                                         .zoom(13)                                                                         )
+                                                                         .build()), 1, null);
+            }
+        }
     }
 }
